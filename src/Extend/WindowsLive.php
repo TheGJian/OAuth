620dc6fa6da7aa40bb121e72bd8f60201f6fe1f5
TheGJian <TheGJian@outlook.com>
2020-05-29 10:01:30 +0800

    version

---
+++ src/Extend/WindowsLive.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zguangjian
+ * CreateDate: 2020/5/28 16:40
+ * Email: zguangjian@outlook.com
+ */
+
+namespace zguangjian\Extend;
+
+
+use zguangjian\Events\OAuthInterface;
+
+class WindowsLive extends OAuthInterface
+{
+
+}
